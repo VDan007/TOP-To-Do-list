28a57a68ed25ffc536da01405c4312480c92c7a8
Dániel Varjaskéri <varjaskerid@gmail.com>
2022-01-23 23:40:41 +0100

    experiments

--- src/builder.js
+++ src/builder.js
@@ -1,7 +1,20 @@
+function getInfo(){
+    console.log("works");
+const infoBubble = document.createElement("div");
+const container = document.querySelector('#container');
+container.appendChild(infoBubble);
+infoBubble.style.zIndex='5';
+infoBubble.style.height='300px';
+infoBubble.style.width='300px';
+infoBubble.style.backgroundColor='red';
+}
+
 
 export function build(){ 
 const container = document.querySelector('#container');
 const newToDoBtn = document.createElement('button');
-newToDoBtn.textContent ='Button';
+newToDoBtn.textContent ='add ToDo';
+newToDoBtn.addEventListener('click',getInfo);
 container.append(newToDoBtn);
+
 }
